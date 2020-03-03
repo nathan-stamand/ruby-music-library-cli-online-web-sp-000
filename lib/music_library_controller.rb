4c014318af61725c7ee11cbbfddfaf86e1d65e16
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-03 22:35:34 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -102,7 +102,7 @@ class MusicLibraryController
   
   def play_song
     puts "Which song number would you like to play?"
-    song_number = gets.strip.to_i
+    song_number = 5
     if song_number >= 1 && song_number <= list_songs.length
       list = list_songs[song_number - 1].split(" - ")
       song_name = list[1]
@@ -110,7 +110,7 @@ class MusicLibraryController
       puts "Playing #{song_name} by #{artist_name}"
     end
   end 
-  
+  binding.pry
   def exit 
     exit
   end 
