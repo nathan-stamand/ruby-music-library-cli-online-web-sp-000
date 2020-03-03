8652a8af93be3f3e9f1b91e4c375142566646bd5
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-03 23:08:28 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -104,11 +104,18 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     song_number = gets.strip.to_i
     if song_number >= 1 && song_number <= list_songs.length
-      list = list_songs[song_number - 1].split(" - ")
-      song_name = list[1]
-      artist_name = list[0].split(".")[1].strip
-      var = "Playing #{song_name} by #{artist_name}"
-      puts var
+      songs = Songs.all
+      songs.sort{|a, b| a.name <=> b.name}
+      solution = songs[song_number-1]
+      binding.pry
+      
+      
+      
+      # list = list_songs[song_number - 1].split(" - ")
+      # song_name = list[1]
+      # artist_name = list[0].split(".")[1].strip
+      # var = "Playing #{song_name} by #{artist_name}"
+      # puts var
     end
   end 
   
